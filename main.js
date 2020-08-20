a5f0fa968b7c3c382967a7ea76f99431bbaeb958
Matthew Chen <matthewchen@Matthews-MacBook-Pro.local>
2020-08-20 11:08:34 -0700

    made mobile responsive

--- main.js
+++ main.js
@@ -39,18 +39,18 @@ function generateSelection(event) {
   inspirationQuoteBtn.addEventListener("click", populateInspirePage)
 }
 
-//FOR DUMB QUOTE PAGE
+
+//*FOR DUMB QUOTE PAGE*//
 
 function populateDumbPage(event) {
   const currentTitle = document.createElement('h1');
   const currentRows = document.querySelectorAll('.this-one');
 
-  //hide elements
   for (let i = 0; i < currentRows.length; i++) {
     currentRows[i].remove();
 
   }
-  //create quote title
+
   currentTitle.textContent = "Dumb Quote";
   currentTitle.className = "quote-title";
   container.append(currentTitle);
@@ -63,19 +63,21 @@ function populateDumbPage(event) {
   })
 
   function handleSuccess(data) {
-    console.log(data);
     const quoteBox = document.createElement('div');
     const quoteTextContainer = document.createElement('div');
     const quoteText = document.createElement('div');
-    quoteBox.classList.add("quote-box", "row");
+    const quoteRow = document.createElement('div');
+    quoteRow.classList.add('row', 'justify-content-center', 'quote-row')
+    quoteBox.classList.add("quote-box", "row", 'justify-content-center');
     quoteTextContainer.classList.add('quote-text-container')
     quoteText.classList.add("quote-text");
     quoteText.innerText = `${data.value}
 
     -Donald Trump`;
+    quoteRow.append(quoteBox);
     quoteTextContainer.append(quoteText);
     quoteBox.append(quoteTextContainer);
-    container.append(quoteBox);
+    container.append(quoteRow);
   }
 
   function handleError(err) {
@@ -83,24 +85,28 @@ function populateDumbPage(event) {
   }
 
   const createHome = document.createElement('button');
-  createHome.classList.add('return-home', 'btn-css', 'justify-content-center', 'row'); //have to append to a row not a container
+  const homeRow = document.createElement('div');
+  homeRow.classList.add('row', 'justify-content-center', 'home-row')
+  createHome.classList.add('return-home', 'btn-css', 'justify-content-center');
   createHome.textContent = "Return to Home"
   createHome.addEventListener("click", returnHome);
-  container.append(createHome);
+  homeRow.append(createHome)
+  container.append(homeRow);
 
 }
 
-//for inspiring quote
+
+//*FOR INSPIRATION PAGE*//
 
 function populateInspirePage() {
   const currentTitle = document.createElement('h1');
   const currentRows = document.querySelectorAll('.this-one');
-  //hide elements
+
   for (let i = 0; i < currentRows.length; i++) {
     currentRows[i].remove();
 
   }
-  //create quote title
+
   currentTitle.textContent = "Inspiring Quote";
   currentTitle.className = "quote-title";
   container.append(currentTitle);
@@ -113,19 +119,21 @@ function populateInspirePage() {
   })
 
   function handleSuccess(data) {
-    console.log(data);
     const quoteBox = document.createElement('div');
     const quoteTextContainer = document.createElement('div');
     const quoteText = document.createElement('div');
-    quoteBox.classList.add("quote-box", "row");
+    const quoteRow = document.createElement('div');
+    quoteRow.classList.add('row', 'justify-content-center', 'quote-row')
+    quoteBox.classList.add("quote-box", "row", 'justify-content-center');
     quoteTextContainer.classList.add('quote-text-container')
     quoteText.classList.add("quote-text");
     quoteText.innerText = `${data.quote.quoteText}
 
     -${data.quote.quoteAuthor}`;
+    quoteRow.append(quoteBox);
     quoteTextContainer.append(quoteText);
     quoteBox.append(quoteTextContainer);
-    container.append(quoteBox);
+    container.append(quoteRow);
   }
 
   function handleError(err) {
@@ -133,22 +141,26 @@ function populateInspirePage() {
   }
 
   const createHome = document.createElement('button');
+  const homeRow = document.createElement('div');
+  homeRow.classList.add('row', 'justify-content-center', 'home-row')
   createHome.classList.add('return-home', 'btn-css', 'justify-content-center');
   createHome.textContent = "Return to Home"
   createHome.addEventListener("click", returnHome);
-  container.append(createHome);
+  homeRow.append(createHome)
+  container.append(homeRow);
+
 }
 
 
 //*RETUN TO HOME BUTTON*//
 
 function returnHome() {
-  const hideBox = document.querySelector('.quote-box')
-  container.removeChild(hideBox);
+  const hideRow = document.querySelector('.quote-row')
+  container.removeChild(hideRow);
   const titleOfPage = document.querySelector('.quote-title');
   container.removeChild(titleOfPage);
-  const homeButton = document.querySelector('.return-home');
-  container.removeChild(homeButton);
+  const homeRow = document.querySelector('.home-row');
+  container.removeChild(homeRow);
 
   for (let i = 0; i < hideThis.length; i++) {
     hideThis[i].classList.remove("hidden");
