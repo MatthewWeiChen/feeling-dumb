bc684a5e412a2480e8973804e926edb1c8c19b14
Matthew Chen <matthewchen@Matthews-MacBook-Pro.local>
2020-08-21 12:22:33 -0700

    made a showView function to go to next page of selections instead of dom creation

--- main.js
+++ main.js
@@ -2,39 +2,29 @@ const clickHereBtn = document.getElementById('click-here-btn')
 const rows = document.querySelectorAll('.row');
 const hideThis = document.querySelectorAll('.hide-this');
 const container = document.querySelector('.container');
-const selectionInfo = [
-  "Read a Dumb Quote",
-  "Inspirational Quote"
-]
-clickHereBtn.addEventListener('click', generateSelection);
 
-function generateSelection(event) {
-  for (let i = 0; i < hideThis.length; i++) {
-    hideThis[i].classList.add("hidden");
+
+function showView(viewName) {
+  const views = document.querySelectorAll('.view')
+  for (let i = 0; i < views.length; i++) {
+    if (views[i].dataset.viewName === viewName) {
+      views[i].classList.remove('hidden')
+    } else {
+      views[i].classList.add('hidden')
+    }
   }
-
-  for (let i = 0; i < selectionInfo.length; i++) {
-    let rowContainer = document.createElement('div');
-    rowContainer.classList.add("row", "justify-content-center", "this-one");
-    let dumbHeading = document.createElement('h1');
-    dumbHeading.className = "heading-selection";
-    dumbHeading.textContent = selectionInfo[i]
-    let buttonRow = document.createElement('div');
-    buttonRow.classList.add("row", "justify-content-center", "this-one");
-    let topButton = document.createElement('button');
-    topButton.className = "circle-button";
-    topButton.textContent = "Press";
-    rowContainer.appendChild(dumbHeading);
-    buttonRow.appendChild(topButton);
-    container.appendChild(rowContainer);
-    container.appendChild(buttonRow);
-  }
-
-  let dumbQuoteBtn = document.querySelectorAll('button')[1];
-  dumbQuoteBtn.addEventListener("click", populateDumbPage);
-  let inspirationQuoteBtn = document.querySelectorAll('button')[2];
-  inspirationQuoteBtn.addEventListener("click", populateInspirePage)
 }
+
+clickHereBtn.addEventListener('click', () => {
+  showView('selection')
+})
+
+
+//   let dumbQuoteBtn = document.querySelectorAll('button')[1];
+//   dumbQuoteBtn.addEventListener("click", populateDumbPage);
+//   let inspirationQuoteBtn = document.querySelectorAll('button')[2];
+//   inspirationQuoteBtn.addEventListener("click", populateInspirePage)
+// }
 
 
 
